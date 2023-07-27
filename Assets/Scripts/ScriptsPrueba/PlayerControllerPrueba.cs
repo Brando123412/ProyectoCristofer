d4f94721983fe085566f776fb9e9befc26079be6
Brando123412 <130480267+Brando123412@users.noreply.github.com>
2023-07-27 11:14:18 -0500

    PrimerAvanze

---
+++ Assets/Scripts/ScriptsPrueba/PlayerControllerPrueba.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerControllerPrueba : MonoBehaviour
+{
+    NavMeshAgent navMeshAgent;
+
+    void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 targetPosition = GetValidTargetPosition();
+            if (targetPosition != Vector3.zero)
+            {
+                navMeshAgent.destination = targetPosition;
+            }
+        }
+    }
+
+    Vector3 GetValidTargetPosition()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            NavMeshHit navMeshHit;
+            if (NavMesh.SamplePosition(hit.point, out navMeshHit, 1.0f, NavMesh.AllAreas))
+            {
+                return navMeshHit.position;
+            }
+        }
+        return Vector3.zero;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Puerta")){
+            print("xd");
+            transform.position = new Vector3(0,0,0);
+        }
+    }
+}
